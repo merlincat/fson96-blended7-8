8bf10a84ee734e0234570cf7f9d95ec251c20989
Liubov2909 <ostapcuk2909@gmail.com>
2023-12-02 19:34:39 +0200

    task1-2

--- lesson-04/js/module7.js
+++ lesson-04/js/module7.js
@@ -32,3 +32,41 @@ const subTitleEls = document.querySelectorAll('h3')
 
 const liEl = document.querySelector('[data-topic="navigation"]')
 // console.log(liEl);
+liEl.style.backgroundColor = "yellow"
+
+liEl.lastElementChild.textContent = "Я змінив тут текст!"
+
+const currentTopic = "manipulation"
+const topicEl = document.querySelector(`[data-topic="${currentTopic}"]`)
+// console.log(topicEl);
+topicEl.style.backgroundColor = "blue"
+
+
+const compTitleEl = document.querySelector(".completed")
+// console.log(compTitleEl);
+compTitleEl.parentNode.remove()
+
+const paragrafEl = document.createElement("p")
+paragrafEl.textContent = "Об'єктна модель документа (Document Object Model)"
+
+
+titleEl.after(paragrafEl)
+
+const liElement = document.createElement("li")
+const topTitle = document.createElement("h3")
+const pEl = document.createElement("p")
+topTitle.textContent = "Властивість innerHTML"
+pEl.textContent = "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу"
+liElement.appendChild(topTitle)
+liElement.appendChild(pEl)
+// listEl.append(liElement)
+
+// listEl.insertAdjacentHTML("beforeend", liElement)
+
+const markup = `<li>
+    <h3>Властивість innerHTML</h3>
+    <p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу</p>
+</li>`
+listEl.insertAdjacentHTML("beforeend", markup)
+
+listEl.innerHTML = ""
