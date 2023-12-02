1bd902643ceeddbf8092a1b66bfe5128c2e26360
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2023-12-02 17:38:15 +0100

    commit task 1

---
+++ lesson-04/js/module7.js
@@ -0,0 +1,34 @@
+const bodyEl = document.querySelector('body')
+// console.log(bodyEl);
+
+const titleEl = document.querySelector('#title')
+// console.log(titleEl);
+
+const listEl = document.querySelector('.list')
+// console.log(listEl);
+
+const dataTopicEls = document.querySelectorAll('[data-topic]')
+// console.log(dataTopicEls);
+// dataTopicEls.forEach(el => {
+//     console.log(el);
+// })
+
+const dataTopicEl = document.querySelector('[data-topic]')
+// console.log(dataTopicEl);
+// console.log(listEl.firstElementChild);
+// console.log(dataTopicEls[0]);
+
+// console.log(listEl.lastElementChild);
+// console.log(dataTopicEls[dataTopicEls.length - 1]);
+
+// console.log(titleEl.nextElementSibling);
+
+const subTitleEls = document.querySelectorAll('h3')
+
+// subTitleEls.forEach(el => {
+//     el.classList.add('active');
+//     el.style.color = 'green';
+// })
+
+const liEl = document.querySelector('[data-topic="navigation"]')
+// console.log(liEl);
